ea8f2227ebe48fc264074936e12a12ff51e2d47e
GKPavesi <guipavesi@hotmail.com>
2023-06-14 21:40:41 -0300

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,49 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import React from 'react';
+import * as cheerio from 'cheerio';
 
-export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
+async function MatchesPage() {
+    const matches = await getServerSideProps()
+    return (
         <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+            <h1>Matches</h1>
+            <br/>
+            {matches.map((element, index) => (
+                <div key={index}>
+                    <p>{element.teamNames}</p>
+                    <p>{element.matchStart}</p>
+                    <br/>
+                </div>
+            ))}
         </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    );
 }
+
+export async function getServerSideProps() {
+    const response = await fetch(
+        "https://www.uol.com.br/esporte/futebol/central-de-jogos/",
+        {
+            headers: {
+                "User-Agent":
+                    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246",
+            },
+        }
+    );
+    const html = await response.text();
+
+    const $ = cheerio.load(html);
+
+    const date = "17/06";
+    const scrapedMatches = [];
+
+    $(`.match-wrapper[data-cfg*='${date}']`).each((index, element) => {
+        const teamNames = $(element).find(".team-name").map((index, el) => $(el).text().trim()).get();
+        const matchStart = $(element).find(".match-info-hora").text().trim();
+        const joinedTeamNames = teamNames.join(" vs ").trim();
+        console.log(joinedTeamNames);
+        scrapedMatches.push({ teamNames: joinedTeamNames, matchStart });
+    });
+
+    return scrapedMatches
+}
+
+export default MatchesPage;
